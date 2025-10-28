8db63febe1ea403b3f9d40786be379c3076fdbd0
JackMorin25 <morjm-25@rhodes.edu>
2025-10-27 22:17:10 -0500

    added main loop for program

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from content_manager import ContentManager
 from sentiment_analyzer import SentimentManager
+import keyboard
+import time
 
 
 content_manager = ContentManager()
 sentiment_manager = SentimentManager()
 
-#content_manager.getVidIdSearch("Ludwig")
-response = content_manager.listComments("jBx4qIGd7BE")
+while True:
+    if keyboard.read_key() != 'f4':
+        time.sleep(0.1)
+        continue
 
-for item in response['items']:
-    print("msg: " + item['snippet']['topLevelComment']['snippet']['textDisplay'])
-    sentiment_manager.test(item['snippet']['topLevelComment']['snippet']['textDisplay'])
-    
-#sentiment_manager.test()
+    topic= input("please search a topic: ")
+    response = content_manager.getVidIdSearch(topic)
+    for item in response['items']:
+        print("msg: " + item['snippet']['topLevelComment']['snippet']['textDisplay'])
+        sentiment_manager.test(item['snippet']['topLevelComment']['snippet']['textDisplay'])
